96dd279662919b147e49c4bb4a814b93a3b30ad4
RigsFotografia1969 <150492159+RigsFotografia1969@users.noreply.github.com>
2023-11-10 17:58:02 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // MODIFICA ESTA URL POR LA DEL SERVICIO WEB DESPUES DE HABERLO DESPLEGADO
-const AS_URL_BASE = "https://script.google.com/macros/s/AquiVaTuURL/exec";
+const AS_URL_BASE = "https://script.google.com/macros/s/AKfycbwQdpgXGwnMQHuQaXcE9NQALIMOW2stH7r-8uKkviAHf0kI5ewLul-Mxq4IWZPEv7OfUA/exec";
